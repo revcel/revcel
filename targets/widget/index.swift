f9bde56822c1b5d52dac047cbc05237566586129
Marcin Janus <janekmachine@gmail.com>
2025-04-28 09:16:32 +0200

    chore: code review

--- targets/widget/index.swift
+++ targets/widget/index.swift
@@ -5,6 +5,7 @@ import SwiftUI
 struct exportWidgets: WidgetBundle {
   var body: some Widget {
     SmallShortcutWidget()
+    // Swift Charts requires min ios 16.0
     if #available(iOS 16.0, *) {
       MediumAnalyticsWidget()
     }
