5ec67ef4bfe8e31686ec7b9ee005f158a4f1f0e1
FTCHD <144691102+FTCHD@users.noreply.github.com>
2026-01-23 01:12:54 +0700

    iOS widget updates

--- targets/widget/SubscriptionRequiredView.swift
+++ targets/widget/SubscriptionRequiredView.swift
@@ -2,13 +2,19 @@ import Foundation
 import SwiftUI
 
 struct SubscriptionRequiredView: View {
-  var body: some View {
-    HStack {
-      Text("Subscription missing, tap here to enable")
-        .font(.system(size: 18.0, weight: .bold))
-        .foregroundStyle(Color("gray1000"))
-        .multilineTextAlignment(.center)
+   var body: some View {
+        VStack(spacing: 12) {
+            Image("AppIconImage")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 50.0, height: 50.0)
+                .clipShape(Circle())
+            
+          	Text("Subscription missing, tap here to enable")
+                .font(.system(size: 18.0, weight: .bold))
+                .foregroundStyle(Color("gray1000"))
+                .multilineTextAlignment(.center)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
     }
-    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
-  }
 }
